604c9a2d88a2c7b42a6fe1dd466d80d44bef82ca
v.iusiumbeli <slavayus@gmail.com>
2020-02-14 22:33:24 +0300

    71A

---
+++ src/main/kotlin/t71/A.kt
@@ -0,0 +1,10 @@
+package t71
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    for (i in 0..nextInt()) {
+        val it = nextLine()
+        println(if (it.length > 10) "${it.first()}${it.length - 2}${it.last()}" else it)
+    }
+}
