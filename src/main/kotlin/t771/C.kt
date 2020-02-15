8a21379df340cef5921be046568e79e09e456538
v.iusiumbeli <slavayus@gmail.com>
2020-02-15 20:03:00 +0300

    771C

---
+++ src/main/kotlin/t771/C.kt
@@ -0,0 +1,18 @@
+package t771
+
+import java.util.*
+
+fun main() {
+    with(Scanner(System.`in`)) {
+        val n = nextInt()
+        val k = nextInt()
+        val arr = Array(n) { i -> "${(65 + i / 25).toChar()}${(97 + i % 26).toChar()}" }
+        for (i in 0..n - k) {
+            val legal = next() == "YES"
+            if (!legal) {
+                arr[i + k - 1] = arr[i]
+            }
+        }
+        println(arr.joinToString(" "))
+    }
+}
