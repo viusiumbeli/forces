4fec91e3a5d2e468b4f5257834a3a7d36d532ea4
v.iusiumbeli <slavayus@gmail.com>
2020-02-15 20:03:05 +0300

    1292A

---
+++ src/main/kotlin/t1292/A.kt
@@ -0,0 +1,43 @@
+package t1292
+
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val q = sc.nextInt()
+
+    val road = Array(2) { Array(n) { 0 } }
+
+    var barricadesFull = 0
+    for (i in 1..q) {
+        val x = sc.nextInt() - 1
+        val y = sc.nextInt() - 1
+
+        road[x][y] = 1 - road[x][y]
+
+        var barricadeCell = 0
+        for (j in -1..1) {
+            if (y + j < 0 || y + j >= n) continue
+            if (road[1 - x][y + j] == 1) barricadeCell++
+        }
+
+        barricadesFull += barricadeCell * if (road[x][y] == 0) -1 else 1
+        println(
+            if (barricadesFull == 0) {
+                "YES"
+            } else {
+                "NO"
+            }
+
+        )
+    }
+
+}
+
+/*
+5 3
+1 5
+2 4
+1 5
+ */
